2fd1a0eb3e337084ede9d32ba32e366caecd747c
needed <cociorbaandrei@gmail.com>
2024-08-31 23:55:41 +0300

    Implement identifiers and numbers

--- src/token.rs
+++ src/token.rs
@@ -1,37 +1,65 @@
 use std::fmt;
 
 pub enum Token {
-	LeftParen(String, u32, u32),
-	RightParen(String, u32, u32),
-	LeftBrace(String, u32, u32),
-	RightBrace(String, u32, u32),
-	Star(String, u32, u32),
-	Dot(String, u32, u32),
-	Comma(String, u32, u32),
-	Plus(String, u32, u32),
-	Minus(String, u32, u32),
-	Semicolon(String, u32, u32),
-	Divide(String, u32, u32),
-	Error(String),
-	EndOfFile,
+    LeftParen(String, u32, u32),
+    RightParen(String, u32, u32),
+    LeftBrace(String, u32, u32),
+    RightBrace(String, u32, u32),
+    Star(String, u32, u32),
+    Dot(String, u32, u32),
+    Comma(String, u32, u32),
+    Plus(String, u32, u32),
+    Minus(String, u32, u32),
+    Semicolon(String, u32, u32),
+    Divide(String, u32, u32),
+    Equals(String, u32, u32),
+    EqualsEquals(String, u32, u32),
+    Colon(String, u32, u32),
+    Error(String),
+    Number(String, u32, u32, f64),
+    Identifier(String, u32, u32, String),
+    EndOfFile,
 }
 
 impl fmt::Display for Token {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		match self {
-			Token::LeftParen(lexeme, line, col) => write!(f, "{} {} {}", "LEFT_PAREN", lexeme, "null"),
-			Token::RightParen(lexeme, line, col) => write!(f, "{} {} {}", "RIGHT_PAREN", lexeme, "null"),
-			Token::LeftBrace(lexeme, line, col) => write!(f, "{} {} {}", "LEFT_BRACE", lexeme, "null"),
-			Token::RightBrace(lexeme, line, col) => write!(f, "{} {} {}", "RIGHT_BRACE", lexeme, "null"),
-			Token::Star(lexeme, line, col) => write!(f, "{} {} {}", "STAR", lexeme, "null"),
-			Token::Dot(lexeme, line, col) => write!(f, "{} {} {}", "DOT", lexeme, "null"),
-			Token::Comma(lexeme, line, col) => write!(f, "{} {} {}", "COMMA", lexeme, "null"),
-			Token::Plus(lexeme, line, col) => write!(f, "{} {} {}", "PLUS", lexeme, "null"),
-			Token::Minus(lexeme, line, col) => write!(f, "{} {} {}", "MINUS", lexeme, "null"),
-			Token::Semicolon(lexeme, line, col) => write!(f, "{} {} {}", "SEMICOLON", lexeme, "null"),
-			Token::Divide(lexeme, line, col) => write!(f, "{} {} {}", "DIVIDE", lexeme, "null"),
-			Token::Error(err) => write!(f, "{}", err),
-			Token::EndOfFile => { write!(f, "EOF  null") }
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::LeftParen(lexeme, line, col) => {
+                write!(f, "{} {} {}", "LEFT_PAREN", lexeme, "null")
+            }
+            Token::RightParen(lexeme, line, col) => {
+                write!(f, "{} {} {}", "RIGHT_PAREN", lexeme, "null")
+            }
+            Token::LeftBrace(lexeme, line, col) => {
+                write!(f, "{} {} {}", "LEFT_BRACE", lexeme, "null")
+            }
+            Token::RightBrace(lexeme, line, col) => {
+                write!(f, "{} {} {}", "RIGHT_BRACE", lexeme, "null")
+            }
+            Token::Star(lexeme, line, col) => write!(f, "{} {} {}", "STAR", lexeme, "null"),
+            Token::Dot(lexeme, line, col) => write!(f, "{} {} {}", "DOT", lexeme, "null"),
+            Token::Comma(lexeme, line, col) => write!(f, "{} {} {}", "COMMA", lexeme, "null"),
+            Token::Plus(lexeme, line, col) => write!(f, "{} {} {}", "PLUS", lexeme, "null"),
+            Token::Minus(lexeme, line, col) => write!(f, "{} {} {}", "MINUS", lexeme, "null"),
+            Token::Semicolon(lexeme, line, col) => {
+                write!(f, "{} {} {}", "SEMICOLON", lexeme, "null")
+            }
+            Token::Divide(lexeme, line, col) => write!(f, "{} {} {}", "DIVIDE", lexeme, "null"),
+            Token::Number(lexeme, line, col, ident) => {
+                write!(f, "{} {} {}", "NUMBER", lexeme, ident)
+            }
+            Token::Error(err) => write!(f, "{}", err),
+            Token::EndOfFile => {
+                write!(f, "EOF  null")
+            }
+            Token::Identifier(lexeme, line, col, ident) => {
+                write!(f, "{} {} {}", "IDENTIFIER", lexeme, ident)
+            }
+            Token::Equals(lexeme, line, col) => write!(f, "{} {} {}", "EQUALS", lexeme, "null"),
+            Token::EqualsEquals(lexeme, line, col) => {
+                write!(f, "{} {} {}", "EQUALS_EQUALS", lexeme, "null")
+            }
+            Token::Colon(lexeme, line, col) => write!(f, "{} {} {}", "COLON", lexeme, "null"),
+        }
+    }
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,3 +1,7 @@
+use std::array::from_fn;
+use std::borrow::Borrow;
+use std::iter;
+
 use crate::token::Token;
 
 use anyhow::anyhow;
@@ -5,47 +9,75 @@ use anyhow::Ok;
 use anyhow::Result;
 
 pub struct Tokenizer {
-	file_content: String,
+    file_content: String,
 }
 
 impl Tokenizer {
-	pub fn new(file_content: String) -> Tokenizer {
-		return Tokenizer {
-			file_content: file_content,
-		};
-	}
-	pub fn get_tokens(&self) -> Result<Vec<Token>> {
-		let mut tokens: Vec<Token> = Vec::new();
-		let mut line: u32 = 0;
-		let mut col: u32 = 0;
-		for c in self.file_content.chars() {
-			match c {
-				'(' => tokens.push(Token::LeftParen(c.to_string(), line, col)),
-				')' => tokens.push(Token::RightParen(c.to_string(), line, col)),
-				'{' => tokens.push(Token::LeftBrace(c.to_string(), line, col)),
-				'}' => tokens.push(Token::RightBrace(c.to_string(), line, col)),
-				'*' => tokens.push(Token::Star(c.to_string(), line, col)),
-				'.' => tokens.push(Token::Dot(c.to_string(), line, col)),
-				',' => tokens.push(Token::Comma(c.to_string(), line, col)),
-				'+' => tokens.push(Token::Plus(c.to_string(), line, col)),
-				'-' => tokens.push(Token::Minus(c.to_string(), line, col)),
-				';' => tokens.push(Token::Semicolon(c.to_string(), line, col)),
-				'/' => tokens.push(Token::Divide(c.to_string(), line, col)),
-				' ' => {
-					col += 1;
-				}
-				'\n' => {
-					line += 1;
-					col = 0;
-				}
-				_ => {
-					tokens.push(Token::Error(format!("[line 1] Error: Unexpected character: {}", c)));
-					//writeln!("[line 1] Error: Unexpected character: {}", c);
-				}
-			}
-			col += 1;
-		}
-		tokens.push(Token::EndOfFile);
-		Ok(tokens)
-	}
+    pub fn new(file_content: String) -> Tokenizer {
+        return Tokenizer {
+            file_content: file_content,
+        };
+    }
+    pub fn get_tokens(&self) -> Result<Vec<Token>> {
+        let mut tokens: Vec<Token> = Vec::new();
+        let mut iter = self.file_content.chars().peekable();
+        let mut line: u32 = 0;
+        let mut col: u32 = 0;
+        while let Some(ch) = iter.next() {
+            match ch {
+                '(' => tokens.push(Token::LeftParen(ch.to_string(), line, col)),
+                ')' => tokens.push(Token::RightParen(ch.to_string(), line, col)),
+                '{' => tokens.push(Token::LeftBrace(ch.to_string(), line, col)),
+                '}' => tokens.push(Token::RightBrace(ch.to_string(), line, col)),
+                '*' => tokens.push(Token::Star(ch.to_string(), line, col)),
+                '.' => tokens.push(Token::Dot(ch.to_string(), line, col)),
+                ',' => tokens.push(Token::Comma(ch.to_string(), line, col)),
+                '+' => tokens.push(Token::Plus(ch.to_string(), line, col)),
+                '-' => tokens.push(Token::Minus(ch.to_string(), line, col)),
+                ';' => tokens.push(Token::Semicolon(ch.to_string(), line, col)),
+                '/' => tokens.push(Token::Divide(ch.to_string(), line, col)),
+                '=' => tokens.push(Token::Equals(ch.to_string(), line, col)),
+                ':' => tokens.push(Token::Colon(ch.to_string(), line, col)),
+                '1'..='9' => {
+                    let n: f64 = iter::once(ch) // Start with the initial digit
+                        .chain(std::iter::from_fn(|| {
+                            iter.by_ref().next_if(|c| c.is_ascii_digit() || *c == '.')
+                        })) // Chain subsequent digits
+                        .collect::<String>() // Collect them into a String
+                        .parse()?; // Parse the string into an i64
+                    col += n.to_string().len() as u32;
+
+                    // Push the number token
+                    tokens.push(Token::Number(n.to_string(), line, col, n));
+                }
+                'A'..='z' => {
+                    let identifier: String = iter::once(ch)
+                        .chain(std::iter::from_fn(|| {
+                            iter.by_ref().next_if(|c| c.is_alphabetic())
+                        }))
+                        .collect::<String>()
+                        .parse()?;
+                    col += identifier.len() as u32;
+                    tokens.push(Token::Identifier(identifier.clone(), line, col, identifier));
+                }
+                ' ' => {
+                    col += 1;
+                }
+                '\n' => {
+                    line += 1;
+                    col = 0;
+                }
+                _ => {
+                    tokens.push(Token::Error(format!(
+                        "[line 1] Error: Unexpected character: {}",
+                        ch
+                    )));
+                    //writeln!("[line 1] Error: Unexpected character: {}", c);
+                }
+            }
+        }
+
+        tokens.push(Token::EndOfFile);
+        Ok(tokens)
+    }
 }
